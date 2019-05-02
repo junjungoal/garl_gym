497599e1b13164749b21e2ec0092439a40c1908c
junjungoal <junyamada107@gmail.com>
2019-05-02 16:36:41 +0100

    add description in the setting file

--- setup.py
+++ setup.py
@@ -42,12 +42,12 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.1',  # Required
+    version='0.1.2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='A sample Python project',  # Optional
+    description='The environment for Reinforcement Learning with genetic algorithm',  # Optional
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
